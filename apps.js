211bb827988c9bb6cee715da27373fe533978fb0
unknown <m.rajsingh1301@gmail.com>
2024-02-07 10:37:05 +0530

    added routes

--- apps.js
+++ apps.js
@@ -8,67 +8,84 @@ import { setWallpaper } from 'wallpaper';
 const app = express();
 const port = 3000;
 
-const storage = multer.diskStorage({
-  destination: './uploads/temp',
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  }
-});
+app.get('/',(req,res)=>{
+  res.render("home.ejs")
+})
 
-const upload = multer({ storage: storage });
+app.post('/wallpaper',async(req,res)=>{
 
-function deleteImage(filePath) {
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.error(`Error deleting file: ${filePath}`);
-    } else {
-      console.log(`File deleted: ${filePath}`);
+
+
+  const storage = multer.diskStorage({
+    destination: './uploads/temp',
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
     }
   });
-}
 
-async function fetchRandomImage() {
-  try {
-    const response = await axios.get('https://source.unsplash.com/random?night');
-    return response.request.res.responseUrl; 
-  } catch (error) {
-    console.error('Error fetching random image:', error.message);
-    throw error;
+  const upload = multer({ storage: storage });
+
+  function deleteImage(filePath) {
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error(`Error deleting file: ${filePath}`);
+      } else {
+        console.log(`File deleted: ${filePath}`);
+      }
+    });
   }
-}
 
-async function setWallpaperFromUrl(imageUrl) {
-  try {
-    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-
-    const imagePath = './uploads/temp/component.jpg';
-    fs.writeFileSync(imagePath, imageResponse.data);
-
-    // Set wallpaper
-    await setWallpaper(imagePath);
-
-    const expiryTime = 10000;
-
-    setTimeout(() => {
-      deleteImage(imagePath);
-    }, expiryTime);
-
-    console.log('Random image uploaded successfully and set as wallpaper!');
-  } catch (error) {
-    console.error('Error processing image:', error.message);
+  async function fetchRandomImage() {
+    try {
+      const response = await axios.get('https://source.unsplash.com/random?night');
+      return response.request.res.responseUrl; 
+    } catch (error) {
+      console.error('Error fetching random image:', error.message);
+      throw error;
+    }
   }
-}
 
-async function uploadRandomImage() {
-  const imageUrl = await fetchRandomImage();
-  await setWallpaperFromUrl(imageUrl);
-}
+  async function setWallpaperFromUrl(imageUrl) {
+    try {
+      const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
 
-const uploadInterval = 10000; // 1 hour
-setInterval(uploadRandomImage, uploadInterval);
+      const imagePath = './uploads/temp/component.jpg';
+      fs.writeFileSync(imagePath, imageResponse.data);
 
-app.use(express.static('uploads'));
+      // Set wallpaper
+      await setWallpaper(imagePath);
+
+      const expiryTime = 10000;
+
+      setTimeout(() => {
+        deleteImage(imagePath);
+      }, expiryTime);
+
+      console.log('Random image uploaded successfully and set as wallpaper!');
+    } catch (error) {
+      console.error('Error processing image:', error.message);
+    }
+  }
+
+  async function uploadRandomImage() {
+    const imageUrl = await fetchRandomImage();
+    await setWallpaperFromUrl(imageUrl);
+  }
+
+  const uploadInterval = 10000; // 1 hour
+  setInterval(uploadRandomImage, uploadInterval);
+
+  app.use(express.static('uploads'));
+
+  res.redirect("/done");
+
+});
+
+app.get('/done' ,(req,res)=>{
+  res.render("done.ejs")
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
